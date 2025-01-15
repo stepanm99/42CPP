9f3b8ed5eac5fa284a67f8424c84d38e840e58f4
Stepan Melichar <stepan.melichar99@seznam.cz>
2025-01-15 19:24:47 +0100

    ex03 from CPP01 is done

---
+++ CPP01/src/ex03/HumanB.cpp
@@ -0,0 +1,21 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name)
+{
+	this->name = name;
+	this->weapon = NULL;
+}
+
+HumanB::~HumanB()
+{
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+	this->weapon = &weapon;
+}
+
+void HumanB::attack()
+{
+	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+}
