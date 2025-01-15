6f7079d88fb8dd27027f89a85ad41ceb45e19dc7
Stepan Melichar <stepan.melichar99@seznam.cz>
2025-01-15 19:28:29 +0100

    HumanB in ex03 from CPP01 can have weapon as NULL, in which case segfault can happen, now checking for this case when using attack function

--- CPP01/src/ex03/HumanB.cpp
+++ CPP01/src/ex03/HumanB.cpp
@@ -17,5 +17,8 @@ void HumanB::setWeapon(Weapon &weapon)
 
 void HumanB::attack()
 {
-	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	if (this->weapon != NULL)
+		std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	else
+		std::cout << this->name << " attacks with their bare hands" << std::endl;
 }
