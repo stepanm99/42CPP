aafb8464c4d8025cd5258cbcf8964491eeda8839
stepanm99 <stepan.melichar99@seznam.cz>
2025-08-17 20:17:43 +0200

    updating printout from the ex06 so it looks more like in the subject

--- CPP01/src/ex06/Harl.cpp
+++ CPP01/src/ex06/Harl.cpp
@@ -10,28 +10,28 @@ Harl::~Harl()
 
 void Harl::debug()
 {
-	std::cout << "DEBUG: I love having extra bacon for my \
+	std::cout << "I love having extra bacon for my \
 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"\
 	<< std::endl;
 }
 
 void Harl::info()
 {
-	std::cout << "INFO: I cannot believe adding extra bacon costs more money. \
+	std::cout << "I cannot believe adding extra bacon costs more money. \
 You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking \
 for more!" << std::endl;
 }
 
 void Harl::warning()
 {
-	std::cout << "WARNING: I think I deserve to have some extra bacon for free. \
+	std::cout << "I think I deserve to have some extra bacon for free. \
 I’ve been coming for years whereas you started working here since \
 last month." << std::endl;
 }
 
 void Harl::error()
 {
-	std::cout << "ERROR: This is unacceptable! I want to speak to the manager \
+	std::cout << "This is unacceptable! I want to speak to the manager \
 now." << std::endl;
 }
 
